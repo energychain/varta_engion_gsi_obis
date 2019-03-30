5503bfc96220fd48b7f3e3e6ccd56fd4c612ff5d
Thorsten Zoerner <me@thorsten-zoerner.com>
2019-03-30 13:29:21 +0100

    second RC

---
+++ README.md
@@ -0,0 +1,5 @@
+# varta_engion_gsi_obis
+Decorates VARTA Engion Element(tested with 6) Meters with GrünstromIndex related  OBIS Codes (1.8.1 and 1.8.2). Returns decorated results
+
+
+Part of the Corrently EcoSystem ( https://www.corrently.de )
--- index.js
+++ index.js
@@ -4,22 +4,31 @@
 require('dotenv').config();
 
 let GSI = require("./lib/gsi_varta.js");
+let zip = '69256';
 
-if(process.argv.length>2) {
-  let instance = new GSI({zip:'69256'});
+let argbase=0;
+if(typeof process.env.ZIP != "undefined") {
+  zip = process.env.ZIP;
+  argbase=-1;
+}
+if(process.argv.length>2+argbase) {
+    zip = process.argv[2+argbase];
+}
+if(process.argv.length>3) {
+  let instance = new GSI({zip:zip});
   let subquery = null;
-  if(process.argv.length>3) subquery = process.argv[3];
-  instance.meter(process.argv[2],subquery).then(function(meter) {
+  if(process.argv.length>4+argbase) subquery = process.argv[4+argbase];
+  instance.meter(process.argv[3+argbase],subquery).then(function(meter) {
     console.log(meter);
   });
 } else
 if(typeof process.env.DEVICE_IP != "undefined") {
-  let instance = new GSI({zip:'69256'});
+  let instance = new GSI({zip:zip});
   instance.meter(process.env.DEVICE_IP,null).then(function(meter) {
     console.log(meter);
   });
 } else {
-  let instance = new GSI({zip:'69256'});
+  let instance = new GSI({zip:zip});
   instance.meters().then(function(meters) {
     console.log(meters);
   });
